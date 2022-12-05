35fa48b0059eb32383422d48fa4b38de6693b862
Krzysztof Kokot <krzykok829@student.polsl.pl>
2022-12-05 21:02:36 +0100

    lvl 3 env

---
+++ app.js
@@ -0,0 +1,90 @@
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+
+/************* database *************/
+const connectionLInk = "mongodb://127.0.0.1:27017/";
+//const connectionLInk="mongodb+srv://admin-krzysiek:Test123@cluster0.rqzo4cz.mongodb.net/";
+const dbName = "userDB";
+const collection = "Users";
+mongoose.connect(connectionLInk + dbName);
+
+//schemas
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
+//model
+const User = mongoose.model(collection, userSchema);
+
+
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+app.get("/logout"), function (req, res) {
+    res.render("/");
+}
+
+app.post("/register", function (req, res) {
+
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save(function (err) {
+        if (!err) {
+            res.render("secrets");
+        } else {
+            console.log(err);
+        }
+    })
+});
+app.post("/login", function (req, res) {
+
+    const username = req.body.username;
+    const password = req.body.password;
+    const query = { email: username };
+    User.findOne(query, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password)
+                    res.render("secrets");
+            }
+        }
+    });
+});
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+app.listen(port, function () {
+    console.log("Server is running on port " + port);
+});
